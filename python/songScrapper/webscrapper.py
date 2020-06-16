a5cdc3bb86cc25c06c9ab30205e9e5a7710f2b8c
Mark Whang <hwhang0917@gmail.com>
2020-06-16 22:45:23 +0900

    Fix webscrapper korean title bug

--- python/songScrapper/webscrapper.py
+++ python/songScrapper/webscrapper.py
@@ -4,6 +4,11 @@ from vidscrapper import get_youtube_links
 
 NAMU_URL = "https://namu.wiki/w/%EB%8F%99%EB%AC%BC%EC%9D%98%20%EC%88%B2%20%EC%8B%9C%EB%A6%AC%EC%A6%88/%EB%85%B8%EB%9E%98%20%EB%AA%A9%EB%A1%9D"
 
+def isAnchor(title):
+    if "<a" in title:
+        return True
+    else:
+        return False
 
 def get_songs():
     songs = []
@@ -30,7 +35,11 @@ def get_songs():
         song_title_td = song_tr.find_all("td")[1]
         song["thumbnail"] = str(song_tr.find("img")["src"])
         title_div = song_title_td.find("div", {"class": "wiki-paragraph"})
-        song["kor_title"] = str(title_div.contents[0])
+        if isAnchor(str(title_div.contents[0])):
+            song["kor_title"] = str(title_div.contents[0].text)
+        else:
+            song["kor_title"] = str(title_div.contents[0])
+        print(song["kor_title"])
         song["eng_title"] = str(title_div.find_all(
             "span", {"class": "wiki-size"})[-1].get_text(strip=True))
         song["youtube"] = get_youtube_links(song["eng_title"])
